2fddbf0c611da9743dee9e2df5e826b4e00b69f1
REY Florian <florianrey.info@gmail.com>
2018-09-10 19:26:42 +0200

    les listes

---
+++ Scripts/001_le_tutoriel_python/s003_les_listes.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Les listes
+
+Quelques exemples de manipulations et copies des listes
+"""
+
+import copy
+
+liste = [[1, 2], [2, 3], 4]   # initialise une liste 
+
+print(liste[0])                     # retourne le premier élément
+print(liste[-1])                    # retourne le dernier élément
+print(liste[1:], end="\n\n")        # retourne une liste en partant du 2nd élément
+
+
+
+
+# copie de liste
+liste1 = liste          # renvoie une copie par référence de la liste, pointe donc vers le même objet
+
+liste2 = liste[:]       # renvoie une copie superficielle de la liste,
+                        # équivaux à liste.copy(), ou encore list(liste), 
+                        # ou encore copy.copy(liste)
+
+liste3 = copy.deepcopy(liste)   # renvoie une copie profonde de la liste, nécessite le module copy
+
+# les 'liste2' et 'liste 3' ont de réf différentes, 
+# si les valeurs de premier niveau de 'liste' sont modifiées, 'liste2' et 'liste3' ne seront pas modifiées
+# contrairement à liste1 qui pointe exactement vers les mêmes données
+print(id(liste), "==", id(liste1), "!=", id(liste2), "!=", id(liste3), end="\n\n") 
+# par contre tout le monde pointe vers la même sous liste sauf la copie profonde 'liste3', 
+# qui a une autre instance de liste d'où son nom
+print(id(liste[0]), "==", id(liste1[0]), "==", id(liste2[0]),'!=', id(liste3[0]), end="\n\n") 
+# les valeurs de toute les listes gardent les mêmes réfs tant qu'elles ne sont pas été modifiées
+# même pour la copie profonde
+print(id(liste[0][0]), id(liste1[0][0]), id(liste2[0][0]), id(liste3[0][0])) 
+print("    ↓          ↓          ↓          =    ") 
+liste[0][0] = 1         # la ref ne change que si la valeur est différente
+liste[0][0] = 3         
+print(id(liste[0][0]), id(liste1[0][0]), id(liste2[0][0]), id(liste3[0][0])) 
+print("    =          =                          ") 
+liste[2] = 3            # remarquons que c'est la même ref que la valeur 3
+                        # stockée dans liste[0][0] et liste[1][1]
+print(id(liste[2]), id(liste1[2]), id(liste2[2]), id(liste3[2]), end="\n\n") 
+
+
+# Il y a donc 3 types de copies différentes 
+# cf article => https://www.python-course.eu/python3_deep_copy.php
+
+
+
+
+
+liste2 = [0, 4, 9, 16]
+liste2 += [25, 35]    # concatenation avec une autre liste
+liste2[-1] = 36       # remplacement d'un élément
+liste2.append(64)     # ajout d'un élément à la fin de la liste
+liste2.insert(-1,49)  # ajout d'un élément à l'index donné en paramètre
+print(liste2, end="\n\n")
+
+liste2[2:] = [2, 5]   # supprime tout les élément à partir de la 3eme
+                      # et insère les nouveau éléments
+print(liste2)
+
+liste2.reverse()      # inverse les données
+print(liste2)
+
+liste2.sort()         # tri la liste, ne fonctionne qu'avec des données du même type
+print(liste2, end="\n\n")
+
+liste2.remove(4)             # supprime la premier occurence de 4 trouvé dans la liste
+lastItem = liste2.pop()      # retourne et supprime le dernier élément
+print(lastItem)
+
+print(liste2, "=>", len(liste2), "élements")    # affiche le nombre d'élement d'une liste
+
+liste[:] = []                   # équivaut à liste.clear(), vide la liste
+print(liste, end="\n\n")
+
+# les listes peuvent comporter tout types de données
+liste2dim = [['a', 'b', 3], [1, 2, "e"]]
+print(liste2dim)
+print(liste2dim[1][2])

f437575500327733d4a749e1e9088e4586e96020
REY Florian <florianrey.info@gmail.com>
2018-09-10 22:53:32 +0200

    clean code

--- Scripts/001_le_tutoriel_python/s003_les_listes.py
+++ Scripts/001_le_tutoriel_python/s003_les_listes.py
@@ -30,9 +30,11 @@ liste3 = copy.deepcopy(liste)   # renvoie une copie profonde de la liste, néces
 # si les valeurs de premier niveau de 'liste' sont modifiées, 'liste2' et 'liste3' ne seront pas modifiées
 # contrairement à liste1 qui pointe exactement vers les mêmes données
 print(id(liste), "==", id(liste1), "!=", id(liste2), "!=", id(liste3), end="\n\n") 
+
 # par contre tout le monde pointe vers la même sous liste sauf la copie profonde 'liste3', 
 # qui a une autre instance de liste d'où son nom
 print(id(liste[0]), "==", id(liste1[0]), "==", id(liste2[0]),'!=', id(liste3[0]), end="\n\n") 
+
 # les valeurs de toute les listes gardent les mêmes réfs tant qu'elles ne sont pas été modifiées
 # même pour la copie profonde
 print(id(liste[0][0]), id(liste1[0][0]), id(liste2[0][0]), id(liste3[0][0])) 
@@ -51,48 +53,39 @@ print(id(liste[2]), id(liste1[2]), id(liste2[2]), id(liste3[2]), end="\n\n")
 
 
 
+
+
 liste2 = [0, 4, 9, 16]
 liste2 += [25, 35]    # concatenation avec une autre liste => liste2.extend([25, 35])
-liste2[-1] = 36       # remplacement d'un élément
+liste2[-1] = 36       # remplacement du dernier élément
 liste2.append(64)     # ajout d'un élément à la fin de la liste
 liste2.insert(-1,49)  # ajout d'un élément à l'index donné en paramètre
-print(liste2, end="\n\n")
-
 liste2[2:] = [2, 5]   # supprime tout les élément à partir de la 3eme
                       # et insère les nouveau éléments
-                      
 del liste2[0]         # supprime le 1er élément (index 0)
-
-print(liste2)
-
 liste2.reverse()      # inverse les données
-print(liste2)
-
 liste2.sort()         # tri la liste, ne fonctionne qu'avec des données du même type
-print(liste2, end="\n\n")
-
-liste2.remove(4)             # supprime la premier occurence de 4 trouvé dans la liste
-                             # ValueError si l'élement n'est pas dans la liste
-
-lastItem = liste2.pop()      # retourne et supprime le dernier élément
-print(lastItem)
+liste2.remove(4)      # supprime la premier occurence de 4 trouvé dans la liste
+                      # ValueError si l'élement n'est pas dans la liste
+liste2.pop()          # retourne le dernier élément et le supprime, 
+                      # pratique pour vider une liste
 
 print(liste2, "=>", len(liste2), "élements")    # affiche le nombre d'élement d'une liste
 
-liste[:] = []                   # équivaut à liste.clear(), vide la liste
-print(liste, end="\n\n")
+liste[:] = []         # vide la liste
+                      # équivaut à liste.clear()
+
 
 # les listes peuvent comporter tout types de données
+# par contre, dans ce cas la méthode sort() n'est plus utilisable
 liste2dim = [['a', 'b', 3], [1, 2, "e"]]
 print(liste2dim)
-print(liste2dim[1][2])
-
-# on peut aussi créer un liste à partir de l'objet range
-liste = list(range(5))
-print(liste)
+        
+liste = list(range(5))          # on peut aussi créer une liste 
+print(liste)                    # à partir de l'objet range
 
 liste = [i ** 2 for i in range(1, 30) if i % 2]
 print(liste)
 
-liste[0:10:2] = [3, 4, 0, 6, 10]        # De 0 à 9, change un élément tout les 2 élements 
+liste[0:10:2] = [3, 4, 0, 6, 10]     # De 0 à 9, change un élément tout les 2 élements 
 print(liste)

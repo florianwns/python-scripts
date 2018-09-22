d072d939a0f9931fffac6786251f3ee1d74d1ee8
REY Florian <florianrey.info@gmail.com>
2018-09-22 09:55:07 +0200

    pep8

---
+++ Scripts/001_le_tutoriel_python/s009_PEP8_le_style_de_codage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""PEP8 le style de codage
+
+° Utilisez des indentations de 4 espaces et pas de tabulation.
+
+° Faites en sorte que les lignes ne dépassent pas 79 caractères,
+  au besoin en insérant des retours à la ligne.
+
+° Utilisez des lignes vides pour séparer les fonctions et les classes,
+  ou pour scinder de gros blocs de code à l’intérieur de fonctions.
+
+° Lorsque c’est possible, placez les commentaires sur leur propres lignes.
+
+° Utilisez les chaînes de documentation.
+
+° Utilisez des espaces autour des opérateurs et après les virgules,
+  mais pas juste à l’intérieur des parenthèses : a = f(1, 2) + g(3, 4).
+
+° Nommez toujours vos classes et fonctions de la même manière;
+  la convention est d’utiliser une notation CamelCase pour les classes,
+  et minuscules_avec_trait_bas pour les fonctions et méthodes.
+  Utilisez toujours self comme nom du premier argument des méthodes
+  (voyez Une première approche des classes pour en savoir plus sur
+  les classes et les méthodes).
+
+° N’utilisez pas d’encodage exotique dès lors que votre code est censé
+  être utilisé dans des environnements internationaux.
+  Par défaut, Python travaille en UTF-8.
+  Pour couvrir tous les cas, préférez le simple ASCII.
+
+° De la même manière, n’utilisez que des caractères ASCII
+  pour vos noms de variables s’il est envisageable qu’une personne
+  parlant une autre langue lise ou doive modifier votre code.
+"""
+
+print(__doc__)

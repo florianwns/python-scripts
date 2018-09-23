72bb2f1020577e126019e01cb3f19bc7d93c5e9f
REY Florian <florianrey.info@gmail.com>
2018-09-23 09:37:42 +0200

    add

---
+++ Scripts/001_le_tutoriel_python/s016_les_ensembles.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Les ensembles
+
+Un ensemble est une collection non ordonnée sans élément dupliqué.
+Utile notamment pour la suppression de doublons.
+Les ensembles peuvent aussi gérer des opérations telle que :
+l'union, l'intersection, la différence et la différence symétrique (XOR)
+"""
+
+# initialisation
+fruits = {"banana", "orange", "apple", "orange"}
+print(fruits)
+
+# Vérification de l'existance d'un élément
+print("banana" in fruits)
+
+# initilisation avec le mot clé 'set'
+a = set("bonjour")          #      A                      B
+b = set("journée")          # |          |  o   n   |          |
+                            # |    b     |    u     |   é  e   |
+                            # |          |  j   r   |          |
+
+# lettre dans a mais pas dans b
+print(a - b)      # {'b'}
+
+# lettre dans a OU dans b : UNION
+print(a | b)      # {'b', 'e', 'o', 'n', 'é', 'u', 'r', 'j'}
+
+# lettre dans a ET dans b : INTERSECTION
+print(a & b)      # {'r', 'j', 'u', 'o', 'n'}
+
+# lettre dans a OU dans b MAIS PAS les 2 : XOR
+print(a ^ b)      # {'e', 'é', 'b'}
+
+# XOR équivaut à UNION - INTERSECTION
+print((a | b) - (a & b))    # {'e', 'é', 'b'}

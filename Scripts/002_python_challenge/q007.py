c4eb4456f134bc4c3193041a78cea1b1214313c6
REY Florian <florianrey.info@gmail.com>
2018-09-25 15:53:48 +0200

    decrypt first message

--- Scripts/002_python_challenge/q007.py
+++ Scripts/002_python_challenge/q007.py
@@ -4,4 +4,28 @@
 """Question 007
 
 Download : http://www.pythonchallenge.com/pc/def/oxygen.html
+
+Download the image
+and read data in RGBA Mode
 """
+
+from PIL import Image
+file = Image.open("oxygen.png")
+pixels = list(file.getdata())
+width, height = file.size
+middle = height//2
+
+pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+
+level = None
+text = ""
+for r, g, b, a in pixels[middle]:
+    if r != level:
+        level = r
+        text += chr(level)
+
+print(text)
+
+# smart guy, you made it. the next level is
+# [105, 10, 16, 101, 103, 14, 105, 16, 121]rpngbemkejlfca^_ba_ac
+print("".join(list(map(chr, [105, 10, 16, 101, 103, 14, 105, 16, 121]))))

fa387c7b84d8db5183377f3970b8707e2c8aebb6
REY Florian <florianrey.info@gmail.com>
2018-09-25 16:52:02 +0200

    yep

---
+++ Scripts/002_python_challenge/q008.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Question 008
+
+Source : http://www.pythonchallenge.com/pc/def/integrity.html
+
+Where is the missing link ?
+"""
+import bz2
+
+login = bz2.decompress(b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084')
+password = bz2.decompress(b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08')
+
+print(login.decode(), ':', password.decode())

a3584a606a34feba405f22da8ea7866f537205ae
REY Florian <florianrey.info@gmail.com>
2018-09-25 16:41:19 +0200

    finish 7

--- Scripts/002_python_challenge/q007.py
+++ Scripts/002_python_challenge/q007.py
@@ -11,21 +11,19 @@ and read data in RGBA Mode
 
 from PIL import Image
 file = Image.open("oxygen.png")
-pixels = list(file.getdata())
+data = list(file.getdata())
 width, height = file.size
-middle = height//2
+pixels = [data[i * width:(i + 1) * width] for i in range(height)]
 
-pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
-
-level = None
-text = ""
+level, text, middle = None, "", height//2
 for r, g, b, a in pixels[middle]:
-    if r != level:
+    if r == g and g == b and r != level:
         level = r
         text += chr(level)
-
 print(text)
 
-# smart guy, you made it. the next level is
-# [105, 10, 16, 101, 103, 14, 105, 16, 121]rpngbemkejlfca^_ba_ac
-print("".join(list(map(chr, [105, 10, 16, 101, 103, 14, 105, 16, 121]))))
+# smart guy, you made it. the next level is [105, 10, 16, 101, 103, 14, 105, 16, 121]
+answer = [105, 10, 16, 101, 103, 14, 105, 16, 121]
+print(''.join(chr(i) if i > 100 else chr(i+100) for i in answer))
+
+# integrity

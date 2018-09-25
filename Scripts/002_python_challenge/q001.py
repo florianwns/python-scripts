0dd56068b8a96f18bb9ffc15471b092e5d197c19
REY Florian <florianrey.info@gmail.com>
2018-09-25 12:46:54 +0200

    python challenge

---
+++ Scripts/002_python_challenge/q001.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Question 001
+
+Source : http://www.pythonchallenge.com/pc/def/map.html
+everybody thinks twice before solving this.
+"""
+
+text = """
+g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp.
+bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle.
+sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.
+"""
+
+liste = map(lambda x: x if ord(x) < 97
+            else chr(ord(x) - 24) if ord(x) >= 121
+            else chr(ord(x) + 2), text)
+
+print(''.join(liste))
+
+# i hope you didnt translate it by hand. thats what computers are for.
+# doing it in by hand is inefficient and that's why this text is so long.
+# using string.maketrans() is recommended. now apply on the url.
+
+# map => ocr
+# http://www.pythonchallenge.com/pc/def/ocr.html
---
+++ Scripts/002_python_challenge/q004.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Question 004
+
+Source : http://www.pythonchallenge.com/pc/def/linkedlist.php
+urllib may help. DON'T TRY ALL NOTHINGS, since it will never
+end. 400 times is more than enough.
+"""

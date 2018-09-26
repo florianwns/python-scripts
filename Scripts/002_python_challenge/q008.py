57510b8c5de592042e0970822e6e406e004059a1
REY Florian <florianrey.info@gmail.com>
2018-09-26 11:39:46 +0200

    doc

--- Scripts/002_python_challenge/q008.py
+++ Scripts/002_python_challenge/q008.py
@@ -13,3 +13,6 @@ login = bz2.decompress(b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x
 password = bz2.decompress(b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08')
 
 print(login.decode(), ':', password.decode())
+
+# enter login et password
+# http://www.pythonchallenge.com/pc/return/good.html

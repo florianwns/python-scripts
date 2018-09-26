99eb0b4b1500e7cdfabf55b46fdeb7a5edcd81c2
REY Florian <florianrey.info@gmail.com>
2018-09-26 11:39:57 +0200

    begin evil

---
+++ Scripts/002_python_challenge/q012.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Question 012
+
+Source : http://www.pythonchallenge.com/pc/return/evil.html
+
+dealing evil
+
+you have to download :
+    http://www.pythonchallenge.com/pc/return/evil1.jpg
+    http://www.pythonchallenge.com/pc/return/evil2.jpg
+    http://www.pythonchallenge.com/pc/return/evil3.jpg
+    http://www.pythonchallenge.com/pc/return/evil4.jpg
+"""

1d64f2beaee27cf77017283c2fa3a4eeada2a645
REY Florian <florianrey.info@gmail.com>
2018-09-30 18:01:28 +0200

    doc

--- Scripts/002_python_challenge/q018.py
+++ Scripts/002_python_challenge/q018.py
@@ -8,7 +8,9 @@ Source: http://www.pythonchallenge.com/pc/return/balloons.html
 can you tell the difference?
 it is more obvious that what you might think
 
-the difference is http://www.pythonchallenge.com/pc/return/brightness.html
+the difference is the 'brightness'
+=> http://www.pythonchallenge.com/pc/return/brightness.html
+
 maybe consider deltas.gz
 """
 
@@ -30,6 +32,7 @@ f1 = open("assets/diff1.png", "wb")
 f2 = open("assets/diff2.png", "wb")
 
 for line in diff:
+    print(line)
     bs = bytes([int(o, 16) for o in line[2:].strip().split(" ") if o])
     if line[0] == '+':
         f1.write(bs)

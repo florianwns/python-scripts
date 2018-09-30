8f3767c07080868121d26e3ed08ea03c34c18325
REY Florian <florianrey.info@gmail.com>
2018-09-30 12:37:24 +0200

    start 18

---
+++ Scripts/002_python_challenge/q018.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Question 018
+
+Source: http://www.pythonchallenge.com/pc/return/balloons.html
+
+can you tell the difference?
+it is more obvious that what you might think
+"""

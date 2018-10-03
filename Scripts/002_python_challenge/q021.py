49b8fd74f8b82d049fc6e45b26968372d3524a40
REY Florian <florianrey.info@gmail.com>
2018-10-03 18:22:21 +0200

    __-__

--- Scripts/002_python_challenge/q021.py
+++ Scripts/002_python_challenge/q021.py
@@ -13,3 +13,25 @@ Now for the level:
 * We used to play this game when we were kids
 * When I had no idea what to do, I looked backwards.
 """
+
+import zipfile, zlib, bz2
+
+with zipfile.ZipFile("assets/level_21.zip") as zip_file:
+    zip_file.extractall(pwd=b"redavni", path="assets/level_21")
+
+with open("assets/level_21/package.pack", "rb") as f:
+    data = f.read()
+    result = ''
+    while True:
+        if data.startswith(b'x\x9c'):
+            data = zlib.decompress(data)
+            result += ' '
+        elif data.startswith(b'BZh'):
+            result += '#'
+        elif data.endswith(b'\x9cx'):
+            data = data[::-1]
+            result += '\n'
+        else:
+            break
+    print(data)
+    print(result)

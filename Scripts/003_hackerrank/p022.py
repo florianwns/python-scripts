54a490c9115c48886da61bd8c41a42bce019207b
REY Florian <florianrey.info@gmail.com>
2018-12-28 23:51:59 +0100

    hmmm

---
+++ Scripts/003_hackerrank/p022.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 022
+
+Mutations
+
+Source : https://www.hackerrank.com/challenges/python-mutations/problem
+"""
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position+1:]
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)

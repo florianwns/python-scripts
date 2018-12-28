c5afa1b44e60c757150abd8c5bb0a94c7ba864d1
REY Florian <florianrey.info@gmail.com>
2018-12-28 20:20:37 +0100

    beautify

--- Scripts/003_hackerrank/p018.py
+++ Scripts/003_hackerrank/p018.py
@@ -8,23 +8,12 @@ Detect HTML Tags, Attributes and Attribute Values
 Source : https://www.hackerrank.com/challenges/detect-html-tags-attributes-and-attribute-values/problem
 """
 from html.parser import HTMLParser
-
 class CustomHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print(tag)
-        for attr in attrs:
-            print("-> {} > {}".format(attr[0], attr[1]))
+        [print('-> {} > {}'.format(*attr)) for attr in attrs]
 
-    def handle_startendtag(self, tag, attrs):
-        print(tag)
-        for attr in attrs:
-            print("-> {} > {}".format(attr[0], attr[1]))
-
-html = ""
-for _ in range(int(input())):
-    html += input().rstrip() + "\n"
-
-
+html = '\n'.join([input() for _ in range(int(input()))])
 parser = CustomHTMLParser()
 parser.feed(html)
 parser.close()

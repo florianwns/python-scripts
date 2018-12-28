db61c721f38b7d6bb00916cf0bb124366bd55a30
REY Florian <florianrey.info@gmail.com>
2018-12-28 19:43:49 +0100

    update

--- Scripts/003_hackerrank/p015.py
+++ Scripts/003_hackerrank/p015.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Problem 014
+"""Problem 015
 
 Hex Color Code
 
@@ -9,9 +9,7 @@ Source : https://www.hackerrank.com/challenges/hex-color-code/problem
 """
 import re
 
-regex_hex_color = r"(#[a-fA-F0-9]{1,2}[a-fA-F0-9]{1,2}[a-fA-F0-9]{1,2})"
+regex_hex_color = r"(?!^)(#[a-fA-F0-9]{1,2}[a-fA-F0-9]{1,2}[a-fA-F0-9]{1,2})"
 for _ in range(int(input())):
-    line = input()
-    line = line[1:-1]
-    for hex_color in re.findall(regex_hex_color, line):
+    for hex_color in re.findall(regex_hex_color, input()):
         print(hex_color)

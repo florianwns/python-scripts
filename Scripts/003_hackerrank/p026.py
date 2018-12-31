a998452a1a0ca9f432ac6f09169902c1a2961ff6
REY Florian <florianrey.info@gmail.com>
2018-12-31 12:48:49 +0100

    clean

--- Scripts/003_hackerrank/p026.py
+++ Scripts/003_hackerrank/p026.py
@@ -7,8 +7,6 @@ Text Wrap
 
 Source : https://www.hackerrank.com/challenges/text-wrap/problem
 """
-import textwrap
-
 def wrap(string, max_width):
     l = [string[i:i+max_width] for i in range(0, len(string), max_width)]
     return "\n".join(l)

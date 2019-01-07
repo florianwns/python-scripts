8f498a13e4f39ed24109fe6d8857c4045dd483fb
REY Florian <florianrey.info@gmail.com>
2019-01-07 13:14:22 +0100

    Update Scripts/003_hackerrank/p077.py

---
+++ Scripts/003_hackerrank/p077.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 077
+
+Triangle Quest
+
+Source : https://www.hackerrank.com/challenges/python-quest-1/problem
+"""
+[print((10**i//9)*i) for i in range(1,int(input()))]

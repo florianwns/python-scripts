d80919ef20310113f30c3207d2efb2a3a9178f30
REY Florian <florianrey.info@gmail.com>
2019-01-07 13:14:19 +0100

    Update Scripts/003_hackerrank/p076.py

---
+++ Scripts/003_hackerrank/p076.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 076
+
+Integers Come In All Sizes
+
+Source : https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+"""
+a, b, c, d = (int(input()) for _ in range(4))
+print(a**b + c**d)

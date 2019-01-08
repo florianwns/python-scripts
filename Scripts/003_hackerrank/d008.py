21c66f85f00c817008365e04f58a1c9e91693b0d
REY Florian <florianrey.info@gmail.com>
2019-01-08 16:53:41 +0100

    Update Scripts/003_hackerrank/d008.py

--- Scripts/003_hackerrank/d008.py
+++ Scripts/003_hackerrank/d008.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Day 8: Dictionaries and Maps
+"""Day 9: Recursion 3
 
-Source : https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+Source : https://www.hackerrank.com/challenges/30-recursion/problem
 """
 n = int(input())
 book = dict([input().split() for _ in range(n)])

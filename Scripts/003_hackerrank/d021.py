36642d3079abc8e3bc508fd067e5bdefd8633c5f
OrangePeelFX <florianrey.info@gmail.com>
2019-02-02 10:56:20 +0100

    Create d021.py

---
+++ Scripts/003_hackerrank/d021.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Day 21: Generics
+
+Source : https://www.hackerrank.com/challenges/30-generics/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+"""
+# Made in C#, because Python was not avaible

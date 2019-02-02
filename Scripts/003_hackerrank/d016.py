bee2adbdf96c66047d2f14d9b41ed80c5fd74296
OrangePeelFX <florianrey.info@gmail.com>
2019-02-02 10:01:21 +0100

    Create d016.py

---
+++ Scripts/003_hackerrank/d016.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Day 16: Exceptions - String to Integer
+
+Source : https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
+"""
+try:
+    print(int(input().strip()))
+except:
+    print("Bad String")

b3e466a96a7336eec04ed06011a3d3aaaf878c29
Florian <florianrey.info@gmail.com>
2019-02-04 16:27:19 +0100

    Create p085.py

---
+++ Scripts/003_hackerrank/p085.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 085
+
+Loops
+
+Source : https://www.hackerrank.com/challenges/python-loops/problem
+"""
+print(*[i*i for i in range(int(input()))], sep="\n")

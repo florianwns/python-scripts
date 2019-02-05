3c23a8beb7b87e2a3898d38aac7504985be771e8
Florian <florianrey.info@gmail.com>
2019-02-05 12:08:15 +0100

    Create p092.py

---
+++ Scripts/003_hackerrank/p092.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 092
+
+Map and Lambda Function
+
+Source : https://www.hackerrank.com/challenges/map-and-lambda-expression/problem
+"""
+cube = lambda x: x**3
+
+def fibonacci(n):
+    fib = [0, 1]
+    [fib.append(fib[i-1]+fib[i-2]) for i in range(2, n)]
+    return fib
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))

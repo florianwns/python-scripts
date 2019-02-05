b50df593368f3d137379aad612609d24362f70bb
Florian <florianrey.info@gmail.com>
2019-02-05 12:21:08 +0100

    Create p094.py

---
+++ Scripts/003_hackerrank/p094.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 093
+
+sWAP cASE
+
+Source : https://www.hackerrank.com/challenges/swap-case/problem
+"""
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)

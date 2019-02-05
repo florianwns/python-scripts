47839fb837e8e494de16186b5c05e98a78c40161
Florian <florianrey.info@gmail.com>
2019-02-05 14:04:57 +0100

    Create p100.py

---
+++ Scripts/003_hackerrank/p100.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 100
+
+Input()
+
+Source : https://www.hackerrank.com/challenges/incorrect-regex/problem
+"""
+x, k = map(int, input().split())
+print(eval(input())==k)

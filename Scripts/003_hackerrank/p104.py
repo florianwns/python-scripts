215ea57fadcb472d26de0a4faa0c87f1265a286e
Florian <florianrey.info@gmail.com>
2019-02-05 20:20:16 +0100

    Create p104.py

---
+++ Scripts/003_hackerrank/p104.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 104
+
+Triangle Quest 2
+
+Source : https://www.hackerrank.com/challenges/triangle-quest-2/problem
+"""
+[print(((10**i-1)//9)**2) for i in range(1,int(input())+1)]

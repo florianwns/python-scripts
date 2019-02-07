eb72e429e7736f495e6f96e2b866b6638a2a21c3
Florian <florianrey.info@gmail.com>
2019-02-07 16:21:47 +0100

    Update p114.py

--- Scripts/003_hackerrank/p114.py
+++ Scripts/003_hackerrank/p114.py
@@ -8,7 +8,7 @@ Words Score
 Source : https://www.hackerrank.com/challenges/words-score/problem?
 """
 def score_words(words):
-    return sum([sum(map(word.count, "aeiouy")) % 2 or 2 for word in words])
+    return sum([sum([c in "aeiouy" for c in w]) % 2 or 2 for w in words])
 
 n = int(input())
 words = input().split()

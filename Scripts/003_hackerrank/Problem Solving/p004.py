66d28a64f66774755f6325f0f50a7ed43be636ef
Florian <florianrey.info@gmail.com>
2019-02-07 17:27:08 +0100

    Create p004.py

---
+++ Scripts/003_hackerrank/Problem Solving/p004.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 004
+
+A Very Big Sum
+
+Source : https://www.hackerrank.com/challenges/a-very-big-sum/problem
+"""
+_ = input()
+print(sum(map(int, input().split())))

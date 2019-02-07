e2af3379ae15c8edd0217e8978fe937bcd69dc02
Florian <florianrey.info@gmail.com>
2019-02-07 17:12:46 +0100

    Create p002.py

---
+++ Scripts/003_hackerrank/Problem Solving/p002.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 002
+
+Simple Array Sum
+
+Source : https://www.hackerrank.com/challenges/simple-array-sum/problem
+"""
+_ = input()
+print(sum(map(int, input().split())))

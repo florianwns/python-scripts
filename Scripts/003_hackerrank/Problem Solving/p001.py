1e4fa0a456d8e1b51ecc8882f23dc2ff757e1b34
Florian <florianrey.info@gmail.com>
2019-02-07 17:06:01 +0100

    .

---
+++ Scripts/003_hackerrank/Problem Solving/p001.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 001
+
+Solve Me First
+
+Source : https://www.hackerrank.com/challenges/solve-me-first/problem
+"""
---
+++ Scripts/003_hackerrank/README.md
@@ -0,0 +1,3 @@
+# Python 3 - Tutorial
+
+Voici un liste de scripts qui m'ont permis de prendre en main Python.

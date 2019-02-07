5151085f3c367adb5fb91cbdb58861ceab2944b2
Florian <florianrey.info@gmail.com>
2019-02-07 16:06:15 +0100

    Update p113.py

--- Scripts/003_hackerrank/p113.py
+++ Scripts/003_hackerrank/p113.py
@@ -8,5 +8,5 @@ Find Angle MBC
 Source : https://www.hackerrank.com/challenges/find-angle/problem
 """
 from math import *
-ab, bc = 100, 1
-print(int(degrees(atan(ab/bc))+0.5))
+ab, bc = [int(input()) for _ in range(2)]
+print(str(int(degrees(atan2(ab,bc)) + 0.5))+'°')

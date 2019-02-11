6195e6ebf5b360be2ffae4b10218b13aa6bf9a21
Florian <florianrey.info@gmail.com>
2019-02-11 09:59:21 +0100

    kivy

---
+++ Scripts/006_kivy/s000_installation.sh
@@ -0,0 +1,10 @@
+# Installation de Kivy
+
+# Ressources :
+# - https://kivy.org/doc/stable/installation/installation-osx.html
+# - https://kivy.org/doc/stable/installation/deps-cython.html
+
+brew install pkg-config sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
+pip install Cython==0.28.2
+# or pip install -U --force-reinstall Cython==0.28.2
+pip install kivy
---
+++ Scripts/006_kivy/s001_camera_example.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Camera Example
+==============
+
+Cet exemple montre l'utilisation de la webcam. Cela affiche une fenêtre avec
+un boutton "afficher" pour mettre en marche la caméra et l'éteindre.
+Il se peut que la webcam ne fonctionne pas si gstreamer n'est pas installé, ce
+qui va jeté une exception pendant le traitement du langage kv.
+
+Source : https://kivy.org/doc/stable/examples/gen__camera__main__py.html
+"""
+
+# Décommenter ces lignes afin de voir les messages de débug
+#from kivy.logger import Logger
+#import logging
+#Logger.setLevel(logging.TRACE)
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import time
+Builder.load_string("""
+<CameraClick>:
+    orientation: 'vertical'
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'Play'
+        on_press: camera.play = not camera.play
+        size_hint_y: None
+        height: '48dp'
+    Button:
+        text: 'Capture'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.capture()
+""")
+
+class CameraClick(BoxLayout):
+    def capture(self):
+        """
+        Fonction qui capture les images et leur donne un nom en fonction
+        de l'heure et de la date où la capture a été prise.
+        """
+        camera = self.ids['camera']
+        filename = "IMG_{}.png".format(time.strftime("%Y%m%d_%H%M%S"))
+        camera.export_to_png(filename)
+        print("Capture :", filename)
+
+class TestCamera(App):
+    def build(self):
+        return CameraClick()
+
+TestCamera().run()

5413082e3438611447ec9301458f0d9322916175
Florian <florianrey.info@gmail.com>
2019-02-11 13:18:47 +0100

    Create p012.py

---
+++ Scripts/003_hackerrank/Problem Solving/p012.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 012
+
+Extra Long Factorials
+
+Source : https://www.hackerrank.com/challenges/extra-long-factorials/problem
+"""
+import math
+print(math.factorial(int(input())))

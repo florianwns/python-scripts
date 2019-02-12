b604b389abc9dd8df93e8987f725887f796e2082
Florian <florianrey.info@gmail.com>
2019-02-12 16:31:48 +0100

    Create s005_fm.py

---
+++ Scripts/005_audio/pyo/s005_fm.py
@@ -0,0 +1,19 @@
+"""
+un peu de FM
+
+Source :http://ajaxsoundstudio.com/pyo_on_bela_html/node05.html
+"""
+from pyo import *
+
+s = Server().boot()
+
+fcar = 250
+rat = 0.499
+fmod = fcar * rat
+ind = LFO(freq=1, type=1, mul=.5, add=.5)
+mod = Sine(freq=fmod, mul=fmod*ind*10)
+# la frequence de la variable change de façon modulaire
+# on lui passe un pyobject, idem pour le volume avec mul
+car = Sine(freq=fcar+mod, mul=ind*0.3).out()
+
+s.gui(locals())

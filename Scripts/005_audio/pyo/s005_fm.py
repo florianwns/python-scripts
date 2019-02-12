8efa03032b2ad155ed03b5afb5c4d33c3c4232db
Florian <florianrey.info@gmail.com>
2019-02-12 16:32:42 +0100

    comment

--- Scripts/005_audio/pyo/s005_fm.py
+++ Scripts/005_audio/pyo/s005_fm.py
@@ -13,7 +13,7 @@ fmod = fcar * rat
 ind = LFO(freq=1, type=1, mul=.5, add=.5)
 mod = Sine(freq=fmod, mul=fmod*ind*10)
 # la frequence de la variable change de façon modulaire
-# on lui passe un pyobject, idem pour le volume avec mul
+# on lui passe un PyoObject, idem pour le volume avec mul
 car = Sine(freq=fcar+mod, mul=ind*0.3).out()
 
 s.gui(locals())

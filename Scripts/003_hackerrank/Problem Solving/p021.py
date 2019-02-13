809ff9dd2a18d3ea2dccd70dee21cd27c939e4d2
Florian <florianrey.info@gmail.com>
2019-02-13 10:17:02 +0100

    Update p021.py

--- Scripts/003_hackerrank/Problem Solving/p021.py
+++ Scripts/003_hackerrank/Problem Solving/p021.py
@@ -7,3 +7,11 @@ Divisible Sum Pairs
 
 Source : https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 """
+_, d = map(int, input().split())
+numbers = list(map(int, input().split()))
+nb = len(numbers)
+count = 0
+for i in range(nb-1):
+    for j in range(i+1, nb):
+        count += (numbers[i] + numbers[j]) % d == 0
+print(count)

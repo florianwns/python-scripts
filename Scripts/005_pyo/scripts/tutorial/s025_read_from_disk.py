5eb435aadcf111b39963fa71fb2519e98520b3c5
Florian <florianrey.info@gmail.com>
2019-02-17 11:37:48 +0100

    sndfile

---
+++ Scripts/005_pyo/scripts/tutorial/s025_read_from_disk.py
@@ -0,0 +1,28 @@
+"""
+Lecture de fichier audio à partir du disque
+
+SfPlayer et ses amis lisent les échantillons d'un fichier à partir du disque
+avec un contrôle sur la vitesse de lecture et le bouclage.
+
+Les différents lecteurs :
+- SfPlayer: Lit de nombreux formats de fichiers son à partir du disque.
+- SfMarkerLooper: lit les AIFF avec des repères dans le fichier boucleur.
+- SfMarkerShuffler: lit les AIFF avec un brasseur de fichiers sonores.
+
+Note :
+Lire un fichier son sur un disque peut économiser beaucoup de RAM,
+en particulier si le fichier son est volumineux,
+mais cela coûte plus cher en ressources processeur que de charger le fichier
+son en mémoire lors du premier passage.
+"""
+from pyo import *
+
+s = Server().boot().start()
+
+folder = os.path.dirname(os.path.abspath(__file__)) + "/../../sounds/"
+path = folder + "transparent.aif"
+
+# Lecture en stéréo avec 2 vitesses de lectures différentes à gauche et à droite
+sf = SfPlayer(path, speed=[1, 0.5], loop=True, mul=0.4).out()
+
+s.gui(locals())
---
+++ Scripts/005_pyo/scripts/tutorial/s026_read_from_disk_2.py
@@ -0,0 +1,44 @@
+"""
+Récupérer l'évènement de fin de fichier depuis l'objet SfPlayer.
+
+Cet exemple montre comment utiliser l'évènement de fin de fichier de l'objet
+SfPlayer pour déclencher une autre lecture (éventuellement avec un autre son,
+une autre vitesse, etc.).
+
+Lorsqu'un SfPlayer atteint la fin du fichier, il envoie un déclencheur
+(que vous pourrez reproduire ultérieurement) que l'utilisateur peut récupérer
+avec la syntaxe suivante: variable_name[“trig”]
+"""
+from pyo import *
+import random, os
+
+s = Server().boot().start()
+
+# Banque de sons
+# récupère le dossier le chemin des fichiers sons
+folder = os.path.dirname(os.path.abspath(__file__)) + "/../../sounds/"
+sounds = ["alum1.wav", "alum2.wav", "alum3.wav", "alum4.wav"]
+
+# Crée 2 lecteurs audio, dirigé vers la gauche et la droite
+sfL = SfPlayer(folder+sounds[0], speed=1, mul=0.5).out()
+sfR = SfPlayer(folder+sounds[0], speed=1, mul=0.5).out(1)
+
+# Fonction qui choisit un sons aléatoire pour le lecteur de gauche
+def newL():
+    sfL.path = folder + sounds[random.randint(0, 3)]
+    sfL.speed = random.uniform(0.75, 1.5)
+    sfL.out()
+
+# Le déclencheur du signal "end-of-file"lié à la fonction "newL"
+tfL = TrigFunc(sfL["trig"], newL)
+
+# Fonction qui choisit un sons aléatoire pour le lecteur de droite
+def newR():
+    sfR.path = folder + sounds[random.randint(0, 3)]
+    sfR.speed = random.uniform(0.75, 1.5)
+    sfR.out(1)
+
+# Le déclencheur du signal "end-of-file"lié à la fonction "newR"
+tfR = TrigFunc(sfR["trig"], newR)
+
+s.gui(locals())

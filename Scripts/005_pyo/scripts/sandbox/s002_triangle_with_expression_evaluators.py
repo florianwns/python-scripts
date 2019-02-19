e5ed4e20926d30457190490839fbd115f0d1d13e
Florian <florianrey.info@gmail.com>
2019-02-19 13:11:27 +0100

    Create s002_triangle_with_expression_evaluators.py

---
+++ Scripts/005_pyo/scripts/sandbox/s002_triangle_with_expression_evaluators.py
@@ -0,0 +1,33 @@
+"""
+Un signal trinagulaire avec l'valuateur d'expression
+
+Premier test avec oscillator de frequence
+Algorithme : http://ajaxsoundstudio.com/pyodoc/api/classes/expression.html
+"""
+from pyo import *
+
+s = Server().boot().start()
+
+# Définition du traitement audio
+proc = '''
+(define triangle (
+        (let #ph (~ $1))
+        (- (* (min #ph (- 1 #ph)) 4) 1)
+    )
+)
+triangle $x[0]
+'''
+
+# Signal de fréquence
+freq = Sig(440)
+
+# Slider de controle
+freq.ctrl([SLMap(100, 2000, "lin", "value", 440, dataOnly=True)])
+
+# Appelle le processus en lui passant un signal de fréquence
+triangle = Expr(freq, proc, mul=0.4).out()
+
+# Affichage
+sc = Scope(triangle)
+
+s.gui(locals())

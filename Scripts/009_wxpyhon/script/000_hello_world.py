17543a626477aafc02694677cad93bdda91bc826
Florian <florianrey.info@gmail.com>
2019-02-21 16:21:53 +0100

    rewriting

--- Scripts/009_wxpyhon/script/000_hello_world.py
+++ Scripts/009_wxpyhon/script/000_hello_world.py
@@ -3,17 +3,39 @@
 """
 Le 'hello world' de WxPython
 
-Une application wx minimale. Le passage du contrôle
-de la boucle d'exécution à WxPython.
+Une application wx de base.
 """
 import wx
 
-# Crée une nouvelle fenêtre
-# False pour ne pas rediriger stdout/stderr vers une fenêtre
-app = wx.App(False)
-# Un cadre est une fenêtre de niveau supérieur
-frame = wx.Frame(None, wx.ID_ANY, "Hello World")
+class Bonjour(wx.Frame):
+    """
+    Fenêtre où on affichera le message 'Bonjour tout le monde !'
+    """
+    def __init__(self, titre):
+        super().__init__(None, title=titre, size=(200, 100))
 
-# Affiche le cadre
-frame.Show(True)
-app.MainLoop()
+        # A panel est une fenêtre sur laquelle on place des contrôles
+        # de la taille de son parent
+        conteneur = wx.Panel(self, size=self.GetClientSize())
+
+        # Texte statique centré
+        etiquette = wx.StaticText(conteneur, label="Bonjour tout le monde !")
+        etiquette.Centre()
+
+class HelloWorldApp(wx.App):
+    def OnInit(self):
+        # Creation de la fenêtre
+        fen = Bonjour("Titre de ma fenêtre")
+
+        # Affichage
+        fen.Show()
+
+        # Retourne Vrai pour signaler que tout s'est bien passé
+        return True
+
+if __name__ == '__main__':
+    # On construit notre application
+    app = HelloWorldApp()
+
+    # Boucle infinie de l'application
+    app.MainLoop()

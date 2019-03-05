4e49b47f6c773e603e32b8574a98375ec2ac2bf2
Florian <florianrey.info@gmail.com>
2019-03-05 12:42:17 +0100

    Update s050_nyquist_theorem.py

--- Scripts/005_pyo/scripts/tutorial/s050_nyquist_theorem.py
+++ Scripts/005_pyo/scripts/tutorial/s050_nyquist_theorem.py
@@ -8,7 +8,6 @@ from pyo import *
 # Pour un serveur avec 2 canaux en mode duplex (entrée, sortie)
 # Un taux d'échantillonage de 48Khz et une taille de buffer de 512 échantillons
 s = Server(sr=48000, nchnls=2, buffersize=512, duplex=1)
-s.boot().start()
 
 # Afin de reconstruire un signal, la fréquence d'échantillonage doit être le
 # double de la fréquence du signal échantillonné.
@@ -16,7 +15,5 @@ s.boot().start()
 # Si on tente d'enregistrer un signal de 30Khz dans un convertisseur travaillant
 # en 48Khz, on obtiendra alors un signal de 18khz => 48 - 30 = 18
 
-# la plus haute fréquence pouvant être représentée dans un système en 48Khz
-# est donc sr/2 => 24khz
-
-s.gui(locals())
+# la plus haute fréquence théorique pouvant être représentée dans un système en
+# 48Khz est donc sr/2 => 24khz
